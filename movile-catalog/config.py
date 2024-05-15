dfc64b7945e59e7bf1d292a3ff640472c41e859c
Carlo-MG <145179622+Carlo-MG@users.noreply.github.com>
2024-05-15 15:09:35 -0600

    creacion de la tabla

--- movile-catalog/config.py
+++ movile-catalog/config.py
@@ -2,5 +2,5 @@
 class Config():
     DEBUG = True
     PORT = 4040
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/movile'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/movile'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ movile-catalog/src/models.py
@@ -0,0 +1,6 @@
+from src import db 
+
+class Movile (db.Model):
+    id_movile = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    name = db.Column(db.String(60), index=True, unique=True)
+    description = db.Column(db.String(200), index=True, unique=False)
