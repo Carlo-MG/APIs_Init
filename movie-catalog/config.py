7a76ec62251eb58d0d711cd6de978f05b0f6de79
Carlo-MG <145179622+Carlo-MG@users.noreply.github.com>
2024-05-17 23:07:13 -0600

    ruta protegida en funcionmiento

--- movie-catalog/config.py
+++ movie-catalog/config.py
@@ -1,7 +1,8 @@
 
 class Config():
+    SECRET_KEY = 'hola'
+    JWT_SECRET_KEY = 'hola'
     DEBUG = True
     PORT = 4040
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/movile'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'hola'
--- session-user/src/controller.py
+++ session-user/src/controller.py
@@ -1,38 +1,39 @@
 from src.models import User
 from src import db
 
-
 def create_user(data):
     username = data.get('username')
     email = data.get('email')
     password = data.get('password')
-    
-    
-    if not username: return {'message': 'username undefined'}
-    if not email: return {'message': 'email undefined'}
-    if not password: return {'message': 'password undefined'}
-    
-    user = User.query.filter_by(username=username).all()
-    if user: return {'message': 'nombre de usuario en uso'}
-    user = User.query.filter_by(email=email).all()
-    if user: return {'message': 'correo en uso'}
-    
+
+    if not username:
+        return {'message': 'username undefined'}
+    if not email:
+        return {'message': 'email undefined'}
+    if not password:
+        return {'message': 'password undefined'}
+
+    if User.query.filter_by(username=username).first():
+        return {'message': 'nombre de usuario en uso'}
+    if User.query.filter_by(email=email).first():
+        return {'message': 'correo en uso'}
+
     user = User(username=username, email=email, password=password)
-    
+
     db.session.add(user)
     db.session.commit()
-    
-    return {'message':'OK'}
+
+    return {'message': 'OK'}
 
 def login_by_username(data):
     username = data.get('username')
     password = data.get('password')
-    
-    user = User.query.filter_by(username=username).all()
-    if not user: return {'message': 'usuario sin registrar'}
-    
-    user = User.query.filter_by(username=username).filter_by(password=password).all()
-    if not user: return {'message': 'Contraseña incorrecta'}
-    
-    return {'message': 'OK', 'token': 'hola'}
-    
+
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return {'message': 'usuario sin registrar'}
+
+    if user.password != password:
+        return {'message': 'Contraseña incorrecta'}
+
+    return True
