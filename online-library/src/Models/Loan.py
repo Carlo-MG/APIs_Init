f130a91c47d7d9f5470aae7b830b06094684ffd5
Carlo-MG <145179622+Carlo-MG@users.noreply.github.com>
2024-05-24 22:54:06 -0600

    cambios en los enlases de las tablas

--- online-library/src/Models/Loan.py
+++ online-library/src/Models/Loan.py
@@ -1,14 +1,14 @@
 from src import db
 
 class Loan(db.Model):
-    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Clave foránea al modelo User
     loan_date = db.Column(db.DateTime, nullable=False)
     return_date = db.Column(db.DateTime)
     
+    user = db.relationship('User', back_populates='loans')
+    
     def __repr__(self):
-        return f'<Loan  Book {self.book_id}, Author {self.author_id}'
-    
-    
+        return f'<Loan Book {self.book_id}, Author {self.author_id}>'
--- online-library/src/Models/User.py
+++ online-library/src/Models/User.py
@@ -1,13 +1,11 @@
 from src import db
 
 class User(db.Model):
-    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(60), nullable=False, unique=True)
     password = db.Column(db.String(60), nullable=False, unique=False)
     
-    loans = db.relationship('Loan', backref='user', lazy=True)
+    loans = db.relationship('Loan', back_populates='user', lazy=True)
     
     def __repr__(self) -> str:
-        return f'< User {self.username}>'
-    
+        return f'<User {self.username}>'
